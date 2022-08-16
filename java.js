87f6dca2e36059187cf9780a0c0f8f83a9ebcbe2
Al imran <imranai6111@gmail.com>
2022-08-17 05:00:57 +0600

    second-commit

---
+++ java.js
@@ -0,0 +1,60 @@
+console.log(10);
+console.log(1);
+console.log(10);
+console.log(1);
+console.log(10);
+console.log(1);
+console.log(10);
+console.log(1);
+var money = 10000000;
+var old = 22;
+var wight = 64;
+var money = 1000;
+var player = 10;
+var total = money / player;
+console.log(total);
+var x = 12;
+x = x + 10;
+console.log(x);
+var y = 10;
+y -= 5;
+console.log(y);
+var xy = 5;
+var yx = 5;
+xy++;
+yx--;
+console.log(xy);
+console.log(yx);
+var i = 'hello';
+var j = 'world';
+var name1 = i + ' ' + j;
+console.log(name1);
+var ij = '20';
+var int = parseInt(ij);
+console.log(int);
+//problem -1
+var a = 1000;
+var b = 700;
+var result = a - b;
+console.log(result);
+//problem-2
+var a = 75.25;
+var b = 65;
+var c = 80;
+var d = 35.45;
+var e = 99.50;
+var resultSum = a + b + c + d + e;
+var avarageMarks = resultSum / 5;
+avarageMarks = avarageMarks.toFixed(2);
+avarageMarks = parseFloat(avarageMarks);
+console.log(avarageMarks);
+//problem-3
+var a = 'i am going to be';
+var b = 'an awesome web developer'
+var stringSum = a + ' ' + b;
+console.log(stringSum);
+//problem-4
+var a = 119;
+var b = 5;
+var result = a % b;
+console.log(result);
